24dbfe292fc08109959e21c4d9d56072e302b342
MG1350 <114371390+MG1350@users.noreply.github.com>
2024-09-17 14:52:34 -0400

    Create potatoHead.java

---
+++ potatoHead.java
@@ -0,0 +1,97 @@
+public class potatoHead
+{ 
+
+//instance variables - to be used in all methods in the class 
+private String colorEye;
+private String colorNose;
+private String colorShoe;
+private boolean hasStache;
+private boolean hasHat;
+/*************************************************************************/ 
+
+//constructors 
+//default constructor 
+public potatoHead() 
+{ 
+colorEye = "black";
+colorNose = "brown";
+colorShoe = "black";
+hasStache = true;
+hasHat = true;
+} 
+//initializer/ parameterized constructor 
+public potatoHead(String eye, String nose, String shoe, boolean stache, boolean hat) 
+{ 
+           colorEye = eye;
+	colorNose = nose;
+colorShoe = shoe;
+hasStache = stache;
+hasHat = hat;
+} 
+/******************************************************/ 
+
+//Accessor Methods 
+public String getColorEye() 
+{ 
+return colorEye; 
+} 
+public String getColorNose() 
+{ 
+return colorNose; 
+} 
+public String getColorShoe() 
+{ 
+return colorShoe; 
+} 
+public boolean getHasStache() 
+{ 
+return hasStache; 
+} 
+public boolean getHasHat() 
+{ 
+return hasHat; 
+} 
+/**********************************************************************/ 
+
+//mutator methods 
+public void setColorEye(String color) 
+{ 
+    colorEye = color;
+}
+public void setColorNose(String color) 
+{ 
+    colorNose = color;
+} 
+public void setColorShoe(String color) 
+{ 
+    colorShoe = color;
+} 
+public void setHasStache(boolean state) 
+{ 
+    hasStache = state;
+} 
+public void setHasHat(boolean state) 
+{ 
+    hasHat = state;
+} 
+/********************************************************************/ 
+
+//Other methods 
+/*****************************************************/
+
+
+//toString 
+public String toString() 
+{ 
+return "A message that describes the object and its instance variables"; } 
+/*****************************************************/ //main method 
+public static void main (String [] args) 
+{ 
+//create objects or instances
+//call or access constructors
+//call or access (test) all of the methods
+
+
+
+} 
+}
